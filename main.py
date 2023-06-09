539bfa0fdc0c55f13355e4e62a47f5770912e408
AlbersSoftware <Chrisalberssoftware@gmail.com>
2023-06-09 12:50:28 -0400

    most changes17

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ packageHashTable = Utils.loadPackageData('Data/package_file.csv')
 # Chris Albers, ID:(009877757), C-950 DSA2- C1 requirement
 
 # C2 requirement- Process and flow of the program is determined here in the main. Each section is labled as you go through the CMI as to whats happening.
-
+# time and space complexity of the program is O(n^2)
+# time and space complexity of the program is O(n^2)
 def userinterface():
     pass
 
